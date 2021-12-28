106afde0c92b381437ad02d8c048c4fb14879304
ozkancondek <ozkncndek@gmail.com>
2021-12-28 16:09:37 +0100

    Rlt-30

--- Projects/01-Cities-Project/src/data.js
+++ Projects/01-Cities-Project/src/data.js
@@ -79,4 +79,267 @@ export const data = [
     image:
       "https://images.unsplash.com/photo-1444076784383-69ff7bae1b0a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fGxvd2VyJTIwYW50ZWxvcGUlMjBjYW55b258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
   },
+  {
+    id: 13,
+    title: "PROVINCE OF PANAMA",
+    desc: "The province of Panama is situated east of the famous Panama Canal and is surrounded by the Comarca of Guna Yala (San Blas Islands), the provinces of Darien, Colon, Panama Oeste, as well as the Gulf of Panama.",
+    image:
+      "https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,f_jpg,g_xy_center,h_620,q_65,w_1160,x_1940,y_590/v1/clients/panama/PA_csco_34_f8e2520f-d65f-4372-a99b-14053c8cabdf.jpg",
+  },
+  {
+    id: 14,
+    title: "RIVIERA PACIFICA",
+    desc: " If you’re looking for a resort-style beach vacation, complete with white-sand beaches and beautiful blue water, look no further than Panama`s Riviera Pacifica. ",
+    image:
+      " https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,f_jpg,h_620,q_65,w_1160/v1/clients/panama/_D755931_8829e49c-7b96-4a9a-a849-c8ee4597d5bb.jpg",
+  },
+  {
+    id: 15,
+    title: "Taj Mahal ",
+    desc: "The Taj Mahal was designated an UNESCO World Heritage Site in 1983. It lies in Agra, Uttar Pradesh in northeastern India. Agra has its own airport, which is 8km from the city center.",
+    image:
+      " https://www.jenreviews.com/wp-content/uploads/2017/04/1.-taj-mahal-380045_960_720.webp",
+  },
+  {
+    id: 16,
+    title: "Yaki Point ",
+    desc: " Yaki point is located in the Grand Canyon National Park, Arizona. At an elevation of 2213 m above sea level, it offers panoramic views over the canyon which take your breath away. ",
+    image:
+      " https://www.jenreviews.com/wp-content/uploads/2017/05/2.-Yaki-Point-grand-canyon-1327044_960_720.webp",
+  },
+  {
+    id: 17,
+    title: "Bethlehem",
+    desc: "Why go to Bethlehem is pretty obvious; how is the question. Bethlehem lies 8.89 km due south of Jerusalem, around 3 ½ km within the Palestinian border. ",
+    image:
+      "https://www.jenreviews.com/wp-content/uploads/2017/05/3.-bethlehem-1010828_960_720.webp ",
+  },
+  {
+    id: 18,
+    title: "  Machu Picchu",
+    desc: " Designated one of the New 7 Wonders of the world in 2007, this 15th century Inca citadel was abandoned by the Incas at the time of the Spanish conquistas, so that it was neither discovered nor plundered and destroyed as most other Inca artefacts were.  ",
+    image:
+      "https://www.jenreviews.com/wp-content/uploads/2017/05/4.-Macchu-Picchu-fog-1868536_960_720.webp ",
+  },
+  {
+    id: 19,
+    title: " Great Wall of China ",
+    desc: "The Great Wall of China, which is visible even from the moon, comprises a total of 21,196 kilometres of fortifications stretching from east to west along the historical boundaries of northern China.  ",
+    image:
+      "https://www.jenreviews.com/wp-content/uploads/2017/05/6.-China-the-great-wall-606451__340.webp ",
+  },
+  {
+    id: 20,
+    title: "Ephesus  ",
+    desc: " Ephesus is the most complete ancient city in the world, and a once-in-a-lifetime travel experience. Founded in the 10th century BC by Attic and Ionian Greek colonists, it fell into the hands of the Romans in 129 BC.  ",
+    image:
+      "https://www.jenreviews.com/wp-content/uploads/2017/05/7.-Ephesus-turkey-93263_960_720.webp ",
+  },
+  {
+    id: 21,
+    title: " Angkor Wat ",
+    desc: "This huge early 12th century temple complex is, at 162.6 hectares, the largest religious monument in the world. It is also Cambodias number one tourist attraction, drawing thousands of visitors annually.   ",
+    image:
+      "https://www.jenreviews.com/wp-content/uploads/2017/05/10.-Angkor-Wat-monument-601312_960_720.webp ",
+  },
+  {
+    id: 22,
+    title: "Petra  ",
+    desc: " Also known as “The Rose City” because of the pinkish stone of which it is built, this ancient capital of the Arab Nabataeans was possibly founded as early as 312 BC. ",
+    image:
+      "https://image.geo.de/30129264/t/TJ/v3/w1440/r0/-/petra-jordanien-f-164089584-jpg--73337-.jpg ",
+  },
+  {
+    id: 23,
+    title: " Fernando de Noronha  ",
+    desc: " For paradise islands straight off a picture postcard, the 21 islands which comprise the Fernando de Noronha archipelago are unparalleled. White sand, tropical vegetation, warm climate and the clearest turquoise water imaginable (visibility up to 50 m!) all go to make this a dream destination.  ",
+    image:
+      "https://www.wohin-und-wann.de/site/images/illustration/fernando-de-noronha-archipel_598.jpg ",
+  },
+  {
+    id: 24,
+    title: "Samarkand  ",
+    desc: "Samarkand is not only one of the oldest inhabited cities in Central Asia, it is one of the most beautiful.   ",
+    image:
+      " https://upload.wikimedia.org/wikipedia/commons/0/00/Registan_square_Samarkand.jpg",
+  },
+  {
+    id: 25,
+    title: "  Glowworm Caves",
+    desc: " At Waitomo on the northern half of New Zealand, a magical and mystical experience awaits the traveller; the Glowworm caves.  ",
+    image:
+      " https://i.natgeofe.com/n/3d20190d-10f5-4d0c-87fc-835fb4cc3150/hanging-glowworms-waitmoto-cave-new-zealand_3x2.jpg",
+  },
+  {
+    id: 26,
+    title: " Niagara falls ",
+    desc: " Probably the most famous waterfall in the world, Niagara Falls  which bridge the border between the United States and Canada  actually comprise three waterfalls: the Horseshoe Falls, the American Falls and the Bridal Falls (both in the United States). ",
+    image:
+      "https://cdn.getyourguide.com/img/location/60096cdee428e.jpeg/88.jpg ",
+  },
+  {
+    id: 27,
+    title: " Stonehenge ",
+    desc: " Few places have been the subject of as much speculation as Stonehenge in Wiltshire, UK ",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/0/0f/Stonehenge%2C_Salisbury_retouched.jpg ",
+  },
+  {
+    id: 28,
+    title: "Salar de Uyuni ",
+    desc: " At 10,582 square metres, Bolivia s Salar de Uyuni is the largest salt flat in the world. It comprises the remains of several prehistoric lakes composed of sodium, magnesium and lithium (up to 70% of the world`s lithium resources come from here), with a surface crust of salt measuring a few metres in depth at some points. ",
+    image:
+      "https://cdn.getyourguide.com/img/location/5cf10385811a6.jpeg/88.jpg ",
+  },
+  {
+    id: 29,
+    title: " Kathmandu ",
+    desc: "The capital city of Nepal in ancient times as today, Kathmandu was ranked third of the top ten upcoming travel destinations in 2013.   ",
+    image:
+      " https://www.meiers-weltreisen.de/dam/jcr:540bff0e-00cc-4457-8bef-69c39d1f4e98/nepal-himalaya-kathmandu-haeuser-berge-t-637696304.jpg",
+  },
+  {
+    id: 30,
+    title: " Santorini ",
+    desc: " This well-loved Greek destination combines in a unique way all the attractions of a paradise island with a lively night-life, comfortable hotels, excellent restaurants and locals well-used to catering for tourists!  ",
+    image:
+      "https://greece-moments.com/wp-content/uploads/2019/12/Santorini-Urlaub-Oia-Hotels-Erfahrungen.jpg ",
+  },
+  {
+    id: 31,
+    title: "Island of Skye ",
+    desc: "For mesmerizing natural beauty, the “fairytale island” of Skye is unbeatable.  ",
+    image:
+      "https://www.myhighlands.de/wp-content/uploads/2018/06/Old-Man-of-Storr-Panorama.jpg ",
+  },
+  {
+    id: 32,
+    title: "Golden Ring ",
+    desc: "These ancient towns comprise a kind of living museum of Russian cultural, architectural and religious history and feature many of the famous “onion domes” often associated with Russian architecture; indeed, they number the prettiest and most picturesque locations in Russia. ",
+    image:
+      "https://cdni.rbth.com/rbthmedia/images/2018.03/original/5a9e4c6a85600a6de9791f62.jpg ",
+  },
+  {
+    id: 33,
+    title: "Boracay  ",
+    desc: " The diminutive Boracay island, located in the Visayas archipelago, is just 7 km long and less than 1 km wide.",
+    image:
+      "https://cf.bstatic.com/xdata/images/hotel/max1024x768/72038944.jpg?k=70b50f99a971a664b5e4dd8bd5003013c62348844186cc73ee5fdfedff04ee04&o=&hp=1 ",
+  },
+  {
+    id: 34,
+    title: "Lhasa  ",
+    desc: "Lhasa is the capital city of the Autonomous Region of Tibet and one of the highest cities in the world (3.656 m above sea level). ",
+    image:
+      "https://static1.evcdn.net/images/reduction/119658_w-1200_h-630_q-70_m-crop.jpg ",
+  },
+  {
+    id: 35,
+    title: "Borobudur, Java ",
+    desc: "The largest Buddhist temple in the world, Borobudur in Magelang, Java was built in the 9th century AD and designated an UNESCO World Heritage Site in 1991. ",
+    image:
+      "https://images.rove.me/w_1920,q_85/lhhlfh1qqjrwco0tt7dk/java-borobudur.jpg ",
+  },
+  {
+    id: 36,
+    title: "Las Lajas Sanctuary ",
+    desc: " The Sanctuary of Las Lajas in southern Columbia is a stunning catholic basilica in Gothic Revival style, which connects the two sides of the Guaitara River Canyon by a 130 metre high bridge. ",
+    image:
+      " https://www.westend61.de/images/0001258957pw/las-lajas-sanctuary-elevated-view-narino-departmant-colombia-south-america-RHPLF12189.jpg",
+  },
+  {
+    id: 37,
+    title: "Kaieteur Falls ",
+    desc: "At a height of 226 metres, Kaieteur Falls is the largest single drop waterfall in the world; it is four times higher than Niagara Falls and twice as high as Victoria Falls. ",
+    image:
+      " https://images.rove.me/w_1920,q_85/eguqwzvdyncbfq6hgd3z/guyana-kaieteur-falls.jpg",
+  },
+  {
+    id: 38,
+    title: "Sidi Bou Said ",
+    desc: "Just north of Tunis at the top of a steep cliff lies the picturesque artist town of Sidi Bou Said. The entire town is decorated in white and blue, with flowers hanging from every wall and in every doorway. ",
+    image: "https://cdn.getyourguide.com/img/tour/5e4ff140e2111.jpeg/148.jpg ",
+  },
+  {
+    id: 39,
+    title: "Serengeti  ",
+    desc: "Established in 1951, Tanzania`s famous Serengeti park covers a staggering surface area of 14.750 square metres and is home to over 3.000 lions, 5.000 African bush elephants and over 1.000 Tanzanian cheetahs, as well many other species. ",
+    image:
+      "https://www.meiers-weltreisen.de/dam/jcr:ce6d7ad2-9821-45aa-b883-2af2f9e46382/tansania-serengeti-kilimandscharo-elefanten-t-507185290.jpg ",
+  },
+  {
+    id: 40,
+    title: "Isfahan ",
+    desc: "Evidence suggests that the origins of Isfahan (also Ispahan, Sepahan, Esfahan or Hispahan) go back to the Paleolithic period; twice the capital of Persia, it was once one of the largest cities in the world. ",
+    image:
+      " https://upload.wikimedia.org/wikipedia/commons/d/d6/Naghshe_Jahan_Square_Isfahan_modified.jpg",
+  },
+  {
+    id: 41,
+    title: "Ice Hotel Balea ",
+    desc: "For a once-in-a-lifetime experience, a stay in Romania`s Ice Hotel Balea takes some beating. ",
+    image:
+      "https://romaniatourstore.com/wp-content/uploads/2018/01/Ice-hotel-Romania.jpg ",
+  },
+  {
+    id: 42,
+    title: " Paro Taktsang Monastery  ",
+    desc: "Known as the “Tiger’s Lair” monastery, it literally clings to a sheer cliff at an elevation of 3.120 metres. ",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/1/14/Taktshang_edit.jpg ",
+  },
+  {
+    id: 43,
+    title: " Kyoto ",
+    desc: "The capital of Imperial Japan for over 1.000 years, Kyoto on the island of Honshu encapsulates the essence of Japanese culture and history is a way few other Japanese cities do.  ",
+    image:
+      " https://cdn.getyourguide.com/img/location/5d079d304c0d5.jpeg/88.jpg",
+  },
+  {
+    id: 44,
+    title: " Krakau ",
+    desc: "One of the loveliest cities in central Europe, and the second largest in Poland, Krakau is a true fairytale city.  ",
+    image: "https://image.gala.de/22214954/t/2X/v7/w1440/r1.5/-/krakau01.jpg ",
+  },
+  {
+    id: 45,
+    title: "Sheikh Zayed Mosque",
+    desc: " The Sheikh Zayed Mosque is one of the most-visited attractions in the UAE and one of the most magnificent examples of contemporary Islamic architecture in the world. ",
+    image:
+      " https://visitabudhabi.ae/-/media/project/vad/galleryimages/what-to-see/national-attractions/sheikh-zayed-grand-mosque/sheikh-zayed-grand-mosque-014.jpg",
+  },
+  {
+    id: 46,
+    title: "Northern Lights ",
+    desc: " Mystical and elusive, the Northern Lights  also known as Aurora Borealis  are something everyone should see once in a lifetime.",
+    image: "https://www.expaturm.com/wp-content/uploads/Northern-lights.jpg ",
+  },
+  {
+    id: 47,
+    title: "Luang Prabang ",
+    desc: "A fascinating blend of spiritual, cultural and architectural traditions, Luang Prabang is widely considered to be the most beautiful city in southeastern Asia.  ",
+    image:
+      " https://www.skr.de/fileadmin/_processed_/f/6/blick_ueber_luang_prabang_120dd92191.jpg",
+  },
+  {
+    id: 48,
+    title: " Lourdes ",
+    desc: "Prior to the 17 mystical visions of the sickly peasant girl Bernadette Soubirous in 1858, Lourdes was a modest market village nestling in the foothills of the Pyrenees in southwestern France.  ",
+    image:
+      "https://media.istockphoto.com/photos/rosary-basilica-in-the-evening-in-lourdes-picture-id602326164?k=20&m=602326164&s=612x612&w=0&h=qpoqJUjv6PlCmHH_aVBD4_VOH6kCCapRBXuQvd95E98= ",
+  },
+  {
+    id: 49,
+    title: "  Cliffs of Moher",
+    desc: " The ruggedly beautiful cliffs of Moher in County Clare are world famous; 8 km long and 214 m high, they tower above the Atlantic as an iconic representation of the “Emerald Isle” in all its beauty. ",
+    image:
+      " https://cdn.getyourguide.com/img/location/5b5da4f4e2932.jpeg/88.jpg",
+  },
+  {
+    id: 50,
+    title: " Meknes ",
+    desc: " Founded by the Almoravids (Moroccan Berbers) in the 11th century, Meknes was once the capital of Morocco and is the sixth largest city in the country today. ",
+    image:
+      "https://www.22places.de/images/2020/01/meknes-tour-bab-mansour.jpg ",
+  },
 ];
