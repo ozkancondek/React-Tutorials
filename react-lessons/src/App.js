93271a84397b7f31347871754d57907710710989
ozkancondek <ozkncndek@gmail.com>
2022-02-16 18:10:47 +0100

    custom hook

--- react-lessons/src/App.js
+++ react-lessons/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import { CustomHook } from "./hooks/CustomHook";
 import { UseEffect } from "./hooks/UseEffect";
 
 function App() {
   return (
     <div className="App">
       <UseEffect />
+      <CustomHook />
     </div>
   );
 }
---
+++ react-lessons/src/hooks/CustomHook.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "../../src/App.css";
+
+//I can use hook in hook
+//if i use a custom hook in a component twice, i use two different state piece
+export const CustomHook = () => {
+  //   const [name, setName] = useState("");
+  //   const [age, setAge] = useState("");
+  //   const [email, setEmail] = useState("");
+
+  // iwant to use just one state to all inputs
+
+  const [inputs, setInputs] = useState({ name: "", age: "", email: "" });
+
+  //statefull logig, for all inputs
+  const handleChange = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value }); // find with name and change it with value
+    // [e.target.name] because its key in object
+  };
+  console.log(inputs);
+  return (
+    <div className="content">
+      <h2>custom hook example</h2>
+
+      <label htmlFor="name">
+        Name: <input name="name" value={inputs.name} onChange={handleChange} />
+      </label>
+      <label htmlFor="age">
+        Age: <input name="age" value={inputs.age} onChange={handleChange} />
+      </label>
+      <label htmlFor="email">
+        Email:{" "}
+        <input name="email" value={inputs.email} onChange={handleChange} />
+      </label>
+    </div>
+  );
+};
