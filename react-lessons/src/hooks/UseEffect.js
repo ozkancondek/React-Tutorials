dc8e95c60dbc65a846793e0756afc4dc9797a8ad
ozkancondek <ozkncndek@gmail.com>
2022-02-16 17:39:22 +0100

    useEffect

--- react-lessons/src/hooks/UseEffect.js
+++ react-lessons/src/hooks/UseEffect.js
@@ -8,7 +8,16 @@ export const UseEffect = () => {
   const increase = () => setCounter(counter + 1);
   //useeffect hook
   //useEffect(() => console.log(counter), []); //componentDidMount, no update
-  useEffect(() => console.log(counter), [counter]); //componentDidUpdate,  update in every update depends on variable in  the d. array
+  //useEffect(() => console.log(counter), [counter]); //componentDidUpdate,  update in every update depends on variable in  the d. array
+
+  //componentWillUnmount
+  //   const [timer, setTimer] = useState(0);
+  //   useEffect(() => {
+  //     const myTimer = setInterval(() => {
+  //       setTimer((timer) => timer + 1); // use callback function  or give timer to dependencies list
+  //     }, 1000);
+  //     return () => clearInterval(myTimer); //add return statement to useEffect
+  //   }, []);
 
   return (
     <div className="content">
@@ -16,6 +25,8 @@ export const UseEffect = () => {
       <h3>Counter</h3>
       <h3>{counter}</h3>
       <button onClick={increase}>increase</button>
+      <h3>Timer</h3>
+      {/*   <h3>{timer}</h3> */}
     </div>
   );
 };
