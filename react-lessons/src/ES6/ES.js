d91e7018367bbd8bad812d0c43f8581d7bdb0e12
ozkancondek <ozkncndek@gmail.com>
2022-02-21 22:52:50 +0100

    arrow function

--- react-lessons/src/ES6/ES.js
+++ react-lessons/src/ES6/ES.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../src/App.css";
 
 export const ES6 = () => {
   //shorthand property
@@ -6,8 +7,27 @@ export const ES6 = () => {
   const name = "ozkan";
   const age = 15;
   const user = { name, age };
-  console.log(user);
+  //console.log(user);
   //{name: 'ozkan', age: 15}
 
-  return <div>TemplralLiterals</div>;
+  //arrow function
+
+  console.log(sum(2, 5));
+  function sum(a, b) {
+    return a + b;
+  }
+
+  //i am gonna get an error with arrow function statement
+  //ReferenceError: Cannot access 'divide' before initialization
+
+  // console.log(divide(8, 2));
+
+  const divide = (a, b) => a / b;
+
+  return (
+    <div className="content">
+      <h2>ES6</h2>
+      <h3> </h3>
+    </div>
+  );
 };
